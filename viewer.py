9929a81efe75ab006954a2750a7fb810c445a9c2
suhtoshi <60370880+suhtoshi@users.noreply.github.com>
2020-05-12 15:57:55 -0500

    Add files via upload
    just files i went through to learn some tkinter

---
+++ viewer.py
@@ -0,0 +1,62 @@
+from tkinter import *
+from PIL import ImageTk, Image
+
+root = Tk()
+root.title('Dumb Ass Title')
+root.iconbitmap('alien.ico')
+
+my_img1 = ImageTk.PhotoImage(Image.open("images/aspen.png"))
+my_img2 = ImageTk.PhotoImage(Image.open("images/aspen2.png"))
+my_img3 = ImageTk.PhotoImage(Image.open("images/me1.png"))
+my_img4 = ImageTk.PhotoImage(Image.open("images/me2.png"))
+my_img5 = ImageTk.PhotoImage(Image.open("images/me3.png"))
+
+image_list = [my_img1, my_img2, my_img3, my_img4, my_img5]
+
+my_label = Label(image = my_img1)
+my_label.grid(row=0, column=0, columnspan=3)
+
+def forward(imgage_number):
+	global my_label
+	global button_forward
+	global button_back
+
+	my_label.grid_forget()
+	my_label = Label(image = image_list[imgage_number-1])
+	button_forward  = Button(root, text=">>", command=lambda: forward(imgage_number + 1))
+	button_back = Button(root, text="<<", command=lambda: back(imgage_number - 1))
+
+	if imgage_number == 5:
+		button_forward = Button(root, text=">>", state=DISABLED)
+
+	my_label.grid(row=0, column=0, columnspan=3)
+	button_forward.grid(row=1, column=2)
+	button_back.grid(row=1, column=0)
+
+def back(imgage_number):
+	global my_label
+	global button_forward
+	global button_back
+
+	my_label.grid_forget()
+	my_label = Label(image = image_list[imgage_number-1])
+	button_forward  = Button(root, text=">>", command=lambda: forward(imgage_number + 1))
+	button_back = Button(root, text="<<", command=lambda: back(imgage_number - 1))
+
+	if imgage_number == 1:
+		button_back = Button(root, text="<<", state=DISABLED)
+
+	my_label.grid(row=0, column=0, columnspan=3)
+	button_forward.grid(row=1, column=2)
+	button_back.grid(row=1, column=0)
+
+button_back = Button(root, text="<<", command=back, state=DISABLED)
+button_exit = Button(root, text="Exit Program", command=root.quit)
+button_forward = Button(root, text=">>",command=lambda: forward(2))
+
+button_back.grid(row=1, column=0)
+button_exit.grid(row=1, column=1)
+button_forward.grid(row=1, column=2)
+
+
+root.mainloop()
